4af26015b7386d6963977358d4f37afc2d84963c
YY <z3388638@gmail.com>
2016-09-09 03:58:45 +0800

    easy to customize map center

--- src/container/mapContainer.jsx
+++ src/container/mapContainer.jsx
@@ -11,10 +11,6 @@ export default class MapContainer extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			center: {
-				lat: 23.94905,
-				lng: 120.93033
-			}, 
 			pokemons: [
 				// {
 				// 	spawnPointId: '3468d8399df',
@@ -72,7 +68,7 @@ export default class MapContainer extends React.Component {
 			<Row style={{height}}>
 				<Col md={12} style={{height: '100%', width: '100%'}}>
 					<GoogleMap
-						defaultCenter={this.state.center}
+						defaultCenter={CONFIG.mapCenter}
 						defaultZoom={16}
 						bootstrapURLKeys={{key: CONFIG.googleApiKey}}
 					>
